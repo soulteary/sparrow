6b0861a00e45d63c19ffca218ace5145cdbf153b
soulteary <soulteary@gmail.com>
2023-05-11 11:56:32 +0800

    feat: StreamBuilderManual (wip)

--- components/stream-responser/stream_builder.go
+++ components/stream-responser/stream_builder.go
@@ -1,29 +1,34 @@
 package StreamResponser
 
 import (
-	"regexp"
-	"strings"
 	"time"
 
 	eb "github.com/soulteary/sparrow/components/event-broker"
 	OpenaiAPI "github.com/soulteary/sparrow/connectors/openai-api"
 	"github.com/soulteary/sparrow/internal/datatypes"
 	"github.com/soulteary/sparrow/internal/define"
-	"github.com/soulteary/sparrow/internal/mock"
 )
 
 func StreamBuilder(data datatypes.Conversation, modelSlug string, broker *eb.Broker, input string) bool {
-	var sequences []string
+	messageID, modelSlug := GetBuilderParams(modelSlug)
+	if define.ENABLE_OPENAI_API {
+		sequences := MakeStreamingMessage(OpenaiAPI.Get(input), modelSlug, data.ConversationID, messageID)
+		return MakeStreamingResponse(data, broker, sequences)
+	}
+	sequences := MakeStreamingMessage("The administrator has disabled the export capability of this model.\nProject: [soulteary/sparrow](https://github.com/soulteary/sparrow).\nTalk is Cheap, Let's coding together.", modelSlug, data.ConversationID, messageID)
+	return MakeStreamingResponse(data, broker, sequences)
+}
+
+func StreamBuilderManual(data datatypes.Conversation, modelSlug string, broker *eb.Broker, input string, markAsEnd bool) bool {
+	return false
+}
+
+func GetBuilderParams(modelSlug string) (string, string) {
 	messageID := define.GenerateUUID()
 	if modelSlug == "" {
 		modelSlug = "text-davinci-002-render-sha"
 	}
-	if define.ENABLE_OPENAI_API {
-		sequences = MakeStreamingMessage(OpenaiAPI.Get(input), modelSlug, data.ConversationID, messageID)
-		return MakeStreamingResponse(data, broker, sequences)
-	}
-	sequences = MakeStreamingMessage("The administrator has disabled the export capability of this model.\nProject: [soulteary/sparrow](https://github.com/soulteary/sparrow).\nTalk is Cheap, Let's coding together.", modelSlug, data.ConversationID, messageID)
-	return MakeStreamingResponse(data, broker, sequences)
+	return messageID, modelSlug
 }
 
 func MakeStreamingResponse(data datatypes.Conversation, broker *eb.Broker, sequences []string) bool {
@@ -58,82 +63,4 @@ func MakeStreamingResponse(data datatypes.Conversation, broker *eb.Broker, seque
 		}
 	}()
 	return true
-}
-
-func MakeResponseMessage(text string, modelSlug, conversationID string, messageID string, endTurn bool) datatypes.ConversationMessageGenerated {
-	data := datatypes.ConversationMessageGenerated{
-		ConversationID: conversationID,
-		Message: datatypes.ConversationMessage{
-			ID:      messageID,
-			Author:  mock.CreateMessageUser("assistant"),
-			Content: mock.CreateMessageContent("text", text),
-			Metadata: datatypes.ConversationMessageGeneratedMetaBody{
-				MessageType: "variant",
-				ModelSlug:   modelSlug,
-			},
-			Weight:    1,
-			Recipient: "all",
-			EndTurn:   nil,
-		},
-		Error: nil,
-	}
-
-	if endTurn {
-		data.Message.Metadata = datatypes.ConversationMessageGeneratedMessageMeta{
-			MessageType: "variant",
-			ModelSlug:   modelSlug,
-			FinishDetails: datatypes.ConversationMessageGeneratedMessageMetaType{
-				Type: "stop",
-			},
-		}
-		data.Message.EndTurn = false
-	}
-	return data
-}
-
-func MakeStreamingMessage(text string, modelSlug string, conversationID string, messageID string) (ret []string) {
-	var messages []datatypes.ConversationMessageGenerated
-
-	// Simulate the feeling of waiting for a response
-	message := MakeResponseMessage("", modelSlug, conversationID, messageID, false)
-	for i := 0; i < 3; i++ {
-		messages = append(messages, message)
-	}
-
-	s := ""
-	lines := strings.Split(text, "\n")
-	lastLineId := len(lines) - 1
-	for lineId, line := range lines {
-		r, _ := regexp.Compile(`!\[[^\]]*\]\([^\)]+\)|\[[^\]]+\]\([^\)]+\)|[^!\[\]\(\)\n]+|\n`)
-		matches := r.FindAllString(line, -1)
-		for _, match := range matches {
-			if ContainMarkdownImage(match) || ContainMarkdownLink(match) {
-				s += match
-				messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, false))
-			} else {
-				context := strings.Split(match, "")
-				for i := 0; i < len(context); i++ {
-					s += context[i]
-					messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, false))
-				}
-			}
-		}
-
-		// before last line
-		if lineId != lastLineId {
-			s += "\n"
-			messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, false))
-		} else {
-			messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, true))
-		}
-	}
-
-	for _, message := range messages {
-		text, err := MakeJSON(message)
-		if err == nil {
-			ret = append(ret, " "+text)
-		}
-	}
-	ret = append(ret, "[DONE]")
-	return ret
 }
---
+++ components/stream-responser/stream_message.go
@@ -0,0 +1,87 @@
+package StreamResponser
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/soulteary/sparrow/internal/datatypes"
+	"github.com/soulteary/sparrow/internal/mock"
+)
+
+func MakeResponseMessage(text string, modelSlug, conversationID string, messageID string, endTurn bool) datatypes.ConversationMessageGenerated {
+	data := datatypes.ConversationMessageGenerated{
+		ConversationID: conversationID,
+		Message: datatypes.ConversationMessage{
+			ID:      messageID,
+			Author:  mock.CreateMessageUser("assistant"),
+			Content: mock.CreateMessageContent("text", text),
+			Metadata: datatypes.ConversationMessageGeneratedMetaBody{
+				MessageType: "variant",
+				ModelSlug:   modelSlug,
+			},
+			Weight:    1,
+			Recipient: "all",
+			EndTurn:   nil,
+		},
+		Error: nil,
+	}
+
+	if endTurn {
+		data.Message.Metadata = datatypes.ConversationMessageGeneratedMessageMeta{
+			MessageType: "variant",
+			ModelSlug:   modelSlug,
+			FinishDetails: datatypes.ConversationMessageGeneratedMessageMetaType{
+				Type: "stop",
+			},
+		}
+		data.Message.EndTurn = false
+	}
+	return data
+}
+
+func MakeStreamingMessage(text string, modelSlug string, conversationID string, messageID string) (ret []string) {
+	var messages []datatypes.ConversationMessageGenerated
+
+	// Simulate the feeling of waiting for a response
+	message := MakeResponseMessage("", modelSlug, conversationID, messageID, false)
+	for i := 0; i < 3; i++ {
+		messages = append(messages, message)
+	}
+
+	s := ""
+	lines := strings.Split(text, "\n")
+	lastLineId := len(lines) - 1
+	for lineId, line := range lines {
+		r, _ := regexp.Compile(`!\[[^\]]*\]\([^\)]+\)|\[[^\]]+\]\([^\)]+\)|[^!\[\]\(\)\n]+|\n`)
+		matches := r.FindAllString(line, -1)
+		for _, match := range matches {
+			if ContainMarkdownImage(match) || ContainMarkdownLink(match) {
+				s += match
+				messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, false))
+			} else {
+				context := strings.Split(match, "")
+				for i := 0; i < len(context); i++ {
+					s += context[i]
+					messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, false))
+				}
+			}
+		}
+
+		// before last line
+		if lineId != lastLineId {
+			s += "\n"
+			messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, false))
+		} else {
+			messages = append(messages, MakeResponseMessage(s, modelSlug, conversationID, messageID, true))
+		}
+	}
+
+	for _, message := range messages {
+		text, err := MakeJSON(message)
+		if err == nil {
+			ret = append(ret, " "+text)
+		}
+	}
+	ret = append(ret, "[DONE]")
+	return ret
+}
